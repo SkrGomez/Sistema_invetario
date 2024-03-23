943610ec54563f902366b3b0b8704d3fd6d1cadb
Nicolas <n.velascovelasquez@unicienciabga.edu.co>
2024-03-23 06:53:42 -0500

    login

--- app/src/main/java/com/uniciencia/sistema_invetario/MainActivity.java
+++ app/src/main/java/com/uniciencia/sistema_invetario/MainActivity.java
@@ -1,14 +1,47 @@
 package com.uniciencia.sistema_invetario;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText usernameEditText, passwordEditText;
+    private Button loginButton;
+
+    // Datos de usuario de ejemplo
+    private static final String CORRECT_USERNAME = "usuario";
+    private static final String CORRECT_PASSWORD = "contraseña";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        usernameEditText = findViewById(R.id.username_edit_text);
+        passwordEditText = findViewById(R.id.password_edit_text);
+        loginButton = findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String enteredUsername = usernameEditText.getText().toString();
+                String enteredPassword = passwordEditText.getText().toString();
+
+                if (enteredUsername.equals(CORRECT_USERNAME) && enteredPassword.equals(CORRECT_PASSWORD)) {
+
+                    Toast.makeText(MainActivity.this, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    
+                    Toast.makeText(MainActivity.this, "Nombre de usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
